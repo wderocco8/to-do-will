28d992d3bd878e9a6088eb6c703606af444bfaf4
William De Rocco <willi@MacBook-Pro.local>
2022-08-30 20:11:21 -0400

    initialize repo

---
+++ src/List.js
@@ -0,0 +1,11 @@
+const List = () => {
+    return ( 
+        <div className="list">
+            <h1>Your To-Do-List</h1>
+            <input type = "text" />
+            <button>Add Item</button>
+        </div>
+     );
+}
+ 
+export default List;
---
+++ src/Navigation.js
@@ -0,0 +1,14 @@
+const Navigation = () => {
+    return (  
+        <nav className="navigation">
+            <h1>To-Do-List</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/reset">Reset To-Do_list</a>
+
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navigation;
